dd4f8ef87ae735cef4f8571d0c3bd96a2fd0fcee
Alexander Swerdlow <aswerdlow935@milkenschool.org>
2018-02-21 09:39:32 -0800

    Removed Docs

--- gulpfile.js
+++ gulpfile.js
@@ -6,29 +6,48 @@ var concat = require("gulp-concat");
 var imagemin = require('gulp-imagemin');
 
 gulp.task('copy', function() {
-    gulp.src('src/**')
+  gulp.src('src/**')
     .pipe(gulp.dest('docs'));
 });
 
-gulp.task("minify-html", function(){
-   gulp.src("src/**/*.html")
-   .pipe(minifyHtml())
-   .pipe(gulp.dest('docs'));
+gulp.task("minify-html", function() {
+  gulp.src("src/**/*.html")
+    .pipe(minifyHtml())
+    .pipe(gulp.dest('docs'));
 });
 
-gulp.task("merge-minify-js-css", function(){
-      gulp.src("src/assets/js/*.js")
-     .pipe(uglify())
-     .pipe(gulp.dest("docs/assets/js"));
-      gulp.src("src/assets/css/*.css")
-     .pipe(minifyCss())
-     .pipe(gulp.dest("docs/assets/css"));
+gulp.task("merge-minify-js-css", function() {
+  gulp.src("src/assets/js/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("docs/assets/js"));
+  gulp.src("src/assets/css/*.css")
+    .pipe(minifyCss())
+    .pipe(gulp.dest("docs/assets/css"));
 });
 
-gulp.task("compress-images", function(){
-      gulp.src('src/images/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('docs/images'))
+gulp.task("compress-images", function() {
+  gulp.src('src/images/*')
+    .pipe(imagemin([
+      imagemin.gifsicle({
+        interlaced: true
+      }),
+      imagemin.jpegtran({
+        progressive: true
+      }),
+      imagemin.optipng({
+        optimizationLevel: 5
+      }),
+      imagemin.svgo({
+        plugins: [{
+            removeViewBox: true
+          },
+          {
+            cleanupIDs: false
+          }
+        ]
+      })
+    ]))
+    .pipe(gulp.dest('docs/images'))
 });
 
 
