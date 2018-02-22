3a65d62e110320ff3db9df1ab05e46df34ee380b
Alexander Swerdlow <aswerdlow935@milkenschool.org>
2018-02-21 20:05:26 -0800

    Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var minifyCss = require("gulp-minify-css");
 var uglify = require("gulp-uglify");
 var concat = require("gulp-concat");
 var imagemin = require('gulp-imagemin');
+var imageminPngquant = require('imagemin-pngquant');
+var imageminMozjpeg = require('imagemin-mozjpeg');
 
 gulp.task('copy', function() {
   gulp.src('src/**/*')
@@ -28,6 +30,14 @@ gulp.task("merge-minify-js-css", function() {
 gulp.task("compress-images", function() {
   gulp.src('src/images/*')
     .pipe(imagemin([
+
+      imageminPngquant({
+        speed: 1,
+        quality: 98
+      }),
+      imageminMozjpeg({
+        quality: 90
+      }),
       imagemin.gifsicle({
         interlaced: true
       }),
@@ -51,4 +61,4 @@ gulp.task("compress-images", function() {
 });
 
 
-gulp.task("default", ["copy","minify-html", "merge-minify-js-css", "compress-images"]);
+gulp.task("default", ["copy", "minify-html", "merge-minify-js-css", "compress-images"]);
