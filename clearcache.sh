729b57de8f6ffff195aa29f19c47d428cb0c9ccb
Alexander Swerdlow <35648800+alexanderswerdlow@users.noreply.github.com>
2018-03-26 17:45:09 -0700

    Create clearcache.sh

---
+++ clearcache.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$ZONE/purge_cache" \
+     -H "X-Auth-Email: $AUTH_EMAIL" \
+     -H "X-Auth-Key: $AUTH_KEY" \
+     -H "Content-Type: application/json" \
+     --data '{"purge_everything":true}'
